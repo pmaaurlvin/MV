de77baaf5bcd190cce40236753b2878046534b7c
pmaaurlvin <130178754+pmaaurlvin@users.noreply.github.com>
2023-04-09 21:51:10 +0200

    Delete Models directory

--- Models/Vote.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MV4.Models
-{
-    public class Vote
-    {
-        public string TrackId { get; set; }
-        public int Count { get; set; }
-        public List<string> IPs { get; set; }
-    }
-}
